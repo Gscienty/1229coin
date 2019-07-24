e8cc73f17b924957d825e160021c8acf3d00f257
Gscienty <gaoxiaochuan@hotmail.com>
2019-07-24 05:59:11 -0400

    feat: add save/load node function pointer type

--- include/define.h
+++ include/define.h
@@ -1,6 +1,14 @@
 #ifndef _1229_COIN_DEFINE_H
 #define _1229_COIN_DEFINE_H
 
+#include <openssl/sha.h>
+
+#define _1229_COIN_VERSION 0x01000000
+#define _1229_COIN_NONCE_LEN 40
+
 #define cast(p, s, m) ((s *) (((char *) p) - ((char *) &(((s *) 0)->m))))
+
+typedef int (*save_node_fptr) (void *node_ptr, const char *base);
+typedef int (*load_node_fptr) (void *node_ptr, const char *base, const unsigned char hash[SHA256_DIGEST_LENGTH]);
 
 #endif
