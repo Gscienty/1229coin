e70f9417994b4a08d75a00ea38505c819d5d7360
Gscienty <gaoxiaochuan@hotmail.com>
2019-07-23 23:27:49 -0400

    feat: implements merkle tree proof

--- src/merkle_tree.c
+++ src/merkle_tree.c
@@ -20,8 +20,30 @@ int merkle_tree_node_parent_calc_sha256(merkle_tree_node_t *parent,
 }
 
 int merkle_tree_proof_of_inclusion(const void *msg, const size_t msg_len,
-                                   const hash_pointer_t path[], const size_t path_len)
+                                   const unsigned char lr_flag[], const hash_pointer_t path[],
+                                   const size_t path_len)
 {
-    // TODO
-    return 0;
+    size_t i;
+    unsigned char mid_msg[SHA256_DIGEST_LENGTH * 2];
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    hash_pointer_calc_sha256(hash, msg, msg_len);
+
+    for (i = 1; i < path_len; i++) {
+        if (lr_flag[i - 1] == 0) {
+            memcpy(mid_msg, path[i - 1].hash, SHA256_DIGEST_LENGTH);
+            memcpy(mid_msg + SHA256_DIGEST_LENGTH, hash, SHA256_DIGEST_LENGTH);
+        }
+        else {
+            memcpy(mid_msg, hash, SHA256_DIGEST_LENGTH);
+            memcpy(mid_msg + SHA256_DIGEST_LENGTH, path[i - 1].hash, SHA256_DIGEST_LENGTH);
+        }
+
+        hash_pointer_calc_sha256(hash, mid_msg, SHA256_DIGEST_LENGTH * 2);
+
+        if (memcmp(hash, path[i].hash, SHA256_DIGEST_LENGTH) != 0) {
+            return 0;
+        }
+    }
+
+    return 1;
 }
